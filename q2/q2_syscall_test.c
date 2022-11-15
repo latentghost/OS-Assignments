87434fef27087b0bce89ce8aadd1644b7f4cbdf8
latentghost <akshatg2003@gmail.com>
2022-11-15 21:45:05 +0530

    1.1 + 2

--- q2/q2_syscall_test.c
+++ q2/q2_syscall_test.c
@@ -8,20 +8,17 @@
 
 
 int main(int argc, char *argv[]){
-	float arr[][4] = {{1.1,2.2,3.3,4.4},{1.2,2.3,3.4,4.5},{9.1,8.2,7.3,6.4},{23.23,39.67,12.09,29.08}};
-	float outarr[4][4];
+	int c = 4, r = 3;
+	float arr[][4] = {{1.1,2.2,3.3,4.4},{9.1,8.2,7.3,6.4},{23.23,39.67,12.09,29.08}};
+	float outarr[r][c];
 
-	if(syscall(TWOD_MEMCPY, arr, outarr, 4, 4) != EFAULT){
+	if(syscall(TWOD_MEMCPY, arr, outarr, c, r) != EFAULT){
 		printf("twod_memcpy system call invoked\n");
-		for(int i=0; i<4; i++){
-			for(int j=0; j<4; j++)
-				printf("%f ",outarr[i][j]);
-			printf("\n");
-		}
+		
 		int b;
-		for(int i=0; i<4; i++){
+		for(int i=0; i<r; i++){
 			b = 0;
-			for(int j=0; j<4; j++){
+			for(int j=0; j<c; j++){
 				if(arr[i][j] != outarr[i][j]){
 					b = 1;
 					printf("%i %i\n",i,j);
--- q2/twodmemcpy/twodmemcpy.c
+++ q2/twodmemcpy/twodmemcpy.c
@@ -4,19 +4,16 @@
 
 SYSCALL_DEFINE4(twodmemcpy, float**, src, float**, dest, int, csize, int, rsize)
 {
-	float buffer[4][4];
+	float buffer[rsize][csize];
 
-	for(int i=0; i<rsize; i++){
-		if(__copy_from_user(&buffer[i], &src[i], sizeof(float) * (csize))){
-			return -EFAULT;
-		}
+	if(__copy_from_user(src, buffer, sizeof(float) * (csize * rsize))){
+		return -EFAULT;
 	}
 
-	for(int i=0; i<rsize; i++){
-		if(__copy_to_user(&dest[i], &buffer[i], sizeof(float) * (csize))){
-			return -EFAULT;
-		}
+	if(__copy_to_user(dest, buffer, sizeof(float) * (csize * rsize))){
+		return -EFAULT;
 	}
+
 
 //	for(int i=0; i<rsize; i++){
 //		for(int j=0; j<csize; j++)
