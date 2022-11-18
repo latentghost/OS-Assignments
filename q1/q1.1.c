8fe09f023c863d32f0974c9fdd69544e8f02171f
latentghost <akshat21515@iiitd.ac.in>
2022-11-19 03:19:49 +0530

    1.1

--- q1/q1.1.c
+++ q1/q1.1.c
@@ -6,11 +6,10 @@
 #include <time.h>
 #include <sched.h>
 
+struct timespec s1,f1,s2,f2,s3,f3;
+
 
 void *counta(void *i){
-
-    struct timespec st,fin;
-    clock_gettime(CLOCK_REALTIME,&st);
 
     struct sched_param p = {.sched_priority = -19};
     pthread_setschedparam(pthread_self(),SCHED_OTHER,&p);
@@ -20,26 +19,11 @@ void *counta(void *i){
     ll j = *((ll *) i);
     for(;j<=r;j++);
 
-    clock_gettime(CLOCK_REALTIME,&fin);
-
-    ll s = fin.tv_sec - st.tv_sec;
-    ll ns = fin.tv_nsec - st.tv_nsec;
-
-    if(ns<0){
-        s--;
-        ns += 1000000000L;
-    }
-
-    printf("SCHED_OTHER: %lli.%lli secs\n",s,ns);
-
     return NULL;
 }
 
 
 void *countb(void *i){
-
-    struct timespec st,fin;
-    clock_gettime(CLOCK_REALTIME,&st);
 
     struct sched_param p = {.sched_priority = 20};
     pthread_setschedparam(pthread_self(),SCHED_FIFO,&p);
@@ -49,26 +33,11 @@ void *countb(void *i){
     ll j = *((ll *) i);
     for(;j<=r;j++);
 
-    clock_gettime(CLOCK_REALTIME,&fin);
-
-    ll s = fin.tv_sec - st.tv_sec;
-    ll ns = fin.tv_nsec - st.tv_nsec;
-
-    if(ns<0){
-        s--;
-        ns += 1000000000L;
-    }
-
-    printf("SCHED_FIFO: %lli.%lli secs\n",s,ns);
-
     return NULL;
 }
 
 
 void *countc(void *i){
-
-    struct timespec st,fin;
-    clock_gettime(CLOCK_REALTIME,&st);
 
     struct sched_param p = {.sched_priority = 98};
     pthread_setschedparam(pthread_self(),SCHED_RR,&p);
@@ -77,18 +46,6 @@ void *countc(void *i){
     ll r = x<<32;
     ll j = *((ll *) i);
     for(;j<=r;j++);
-
-    clock_gettime(CLOCK_REALTIME,&fin);
-
-    ll s = fin.tv_sec - st.tv_sec;
-    ll ns = fin.tv_nsec - st.tv_nsec;
-
-    if(ns<0){
-        s--;
-        ns += 1000000000L;
-    }
-
-    printf("SCHED_RR: %lli.%lli secs\n",s,ns);
 
     return NULL;
 }
@@ -99,17 +56,67 @@ int main(int argc, char* argv[]){
     pthread_t ta,tb,tc;
     ll i = 1;
 
+    // Start all threads in parallel
+
+    clock_gettime(CLOCK_REALTIME,&s1);
+
     pthread_create(&ta,NULL,&counta,&i);
 
-    pthread_join(ta,NULL);
+    clock_gettime(CLOCK_REALTIME,&s2);
 
     pthread_create(&tb,NULL,&countb,&i);
 
-    pthread_join(tb,NULL);
+    clock_gettime(CLOCK_REALTIME,&s3);
 
     pthread_create(&tc,NULL,&countc,&i);
 
+
+    // End thread A
+
+    pthread_join(ta,NULL);
+
+    clock_gettime(CLOCK_REALTIME,&f1);
+    
+    ll s,ns;
+
+    s = f1.tv_sec - s1.tv_sec;
+    ns = f1.tv_nsec - s1.tv_nsec;
+    if(ns<0){
+        s--;
+        ns += 1000000000L;
+    }
+    printf("SCHED_OTHER: %lli.%lli seconds\n",s,ns);
+
+
+    // End thread B
+
+    pthread_join(tb,NULL);
+
+    clock_gettime(CLOCK_REALTIME,&f2);
+
+    s = f2.tv_sec - s2.tv_sec;
+    ns = f2.tv_nsec - s2.tv_nsec;
+    if(ns<0){
+        s--;
+        ns += 1000000000L;
+    }
+    printf("SCHED_FIFO: %lli.%lli seconds\n",s,ns);
+
+
+    // End thread C
+
     pthread_join(tc,NULL);
+
+    clock_gettime(CLOCK_REALTIME,&f3);
+
+    s = f3.tv_sec - s3.tv_sec;
+    ns = f3.tv_nsec - s3.tv_nsec;
+    if(ns<0){
+        s--;
+        ns += 1000000000L;
+    }
+    printf("SCHED_RR: %lli.%lli seconds\n",s,ns);
+
 
     return 0;
 
