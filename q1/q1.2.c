df0666b19d909d7b01e56583c27bf56e97899620
latentghost <akshat21515@iiitd.ac.in>
2022-11-19 02:40:02 +0530

    1.2

--- q1/q1.2.c
+++ q1/q1.2.c
@@ -4,16 +4,21 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
-#include <string.h>
+#include <sched.h>
 #include <time.h>
 
 
 struct timespec s1,f1,s2,f2,s3,f3;
+struct sched_param p1 = {.sched_priority = 50};
+struct sched_param p2 = {.sched_priority = 50};
+struct sched_param p3 = {.sched_priority = 50};
 
 
 int main(int argc, char *argv[]){
 
     pid_t pid1, status1;
+
+    sched_setscheduler(pid1,SCHED_OTHER,&p1);
 
     clock_gettime(CLOCK_REALTIME, &s1);
 
@@ -34,6 +39,8 @@ int main(int argc, char *argv[]){
 
         pid_t pid2, status2;
 
+        sched_setscheduler(pid2,SCHED_FIFO,&p2);
+
         clock_gettime(CLOCK_REALTIME, &s2);
 
         pid2 = fork();
@@ -52,6 +59,8 @@ int main(int argc, char *argv[]){
         else{
 
             pid_t pid3, status3;
+
+            sched_setscheduler(pid3,SCHED_RR,&p3);
 
             clock_gettime(CLOCK_REALTIME, &s3);
 
@@ -88,7 +97,7 @@ int main(int argc, char *argv[]){
         s--;
         ns += 1000000000L;
     }
-    printf("Child process 1: %lli.%lli seconds\n",s,ns);
+    printf("Child process 1 (SCHED_OTHER): %lli.%lli seconds\n",s,ns);
 
     s = f2.tv_sec - s2.tv_sec;
     ns = f2.tv_nsec - s2.tv_nsec;
@@ -96,7 +105,7 @@ int main(int argc, char *argv[]){
         s--;
         ns += 1000000000L;
     }
-    printf("Child process 2: %lli.%lli seconds\n",s,ns);
+    printf("Child process 2 (SCHED_FIFO): %lli.%lli seconds\n",s,ns);
 
     s = f3.tv_sec - s3.tv_sec;
     ns = f3.tv_nsec - s3.tv_nsec;
@@ -104,6 +113,6 @@ int main(int argc, char *argv[]){
         s--;
         ns += 1000000000L;
     }
-    printf("Child process 3: %lli.%lli seconds\n",s,ns);
+    printf("Child process 3 (SCHED_RR): %lli.%lli seconds\n",s,ns);
     
 }
