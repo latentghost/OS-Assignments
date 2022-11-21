00666d3fad4746dc39d615d1938f953880654441
latentghost <akshatg2003@gmail.com>
2022-11-22 04:31:27 +0530

    WTF IS WRONG WITH ME

--- q2/twodmemcpy/twodmemcpy.c
+++ q2/twodmemcpy/twodmemcpy.c
@@ -6,20 +6,13 @@ SYSCALL_DEFINE4(twodmemcpy, float**, src, float**, dest, int, csize, int, rsize)
 {
 	float buffer[rsize][csize];
 
-	if(__copy_from_user(src, buffer, sizeof(float) * (csize * rsize))){
+	if(__copy_from_user(buffer, src, sizeof(float) * (csize * rsize))){
 		return -EFAULT;
 	}
 
 	if(__copy_to_user(dest, buffer, sizeof(float) * (csize * rsize))){
 		return -EFAULT;
 	}
-
-
-//	for(int i=0; i<rsize; i++){
-//		for(int j=0; j<csize; j++)
-//			printf("%i ",dest[i][j]);
-//		printf("\n");
-//	}
 
 
 	return 0;
