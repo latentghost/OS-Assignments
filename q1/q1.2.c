ba7560fc49f62c8b0a0f2d3ddcfb25805f30d916
latentghost <akshat21515@iiitd.ac.in>
2022-11-24 14:20:44 +0530

    1.2 fic func

--- q1/q1.2.c
+++ q1/q1.2.c
@@ -13,6 +13,20 @@ struct sched_param p1 = {.sched_priority = 50};
 struct sched_param p2 = {.sched_priority = 50};
 struct sched_param p3 = {.sched_priority = 50};
 
+ll * fix(ll sec, ll nsec){
+
+    if(nsec<0L){
+        sec--;
+        nsec += 1000000000L
+    }
+
+    ll * out = malloc(2*sizeof(ll));
+    *(out) = sec;
+    *(out + 1) = nsec;
+
+    return out
+}
+
 
 int main(int argc, char *argv[]){
 
@@ -30,7 +44,7 @@ int main(int argc, char *argv[]){
     }
 
     else if(pid1==0){
-        execl("/bin/bash","sh","s1.sh",(char *) NULL);
+        // execl("/bin/bash","sh","s1.sh",(char *) NULL);
 
         exit(EXIT_SUCCESS);
     }
@@ -51,7 +65,7 @@ int main(int argc, char *argv[]){
         }
 
         else if(pid2==0){
-            execl("/bin/bash","sh","s2.sh",(char *) NULL);
+            // execl("/bin/bash","sh","s2.sh",(char *) NULL);
 
             exit(EXIT_SUCCESS);
         }
@@ -72,7 +86,7 @@ int main(int argc, char *argv[]){
             }
 
             else if(pid3==0){
-                execl("/bin/bash","sh","s3.sh",(char *) NULL);
+                // execl("/bin/bash","sh","s3.sh",(char *) NULL);
 
                 exit(EXIT_SUCCESS);
             }
@@ -93,26 +107,29 @@ int main(int argc, char *argv[]){
     ll s,ns;
     s = f1.tv_sec - s1.tv_sec;
     ns = f1.tv_nsec - s1.tv_nsec;
-    if(ns < 0L){
-        s--;
-        ns += 1000000000L;
-    }
-    printf("Child process 1 (SCHED_OTHER): %lli.%lli seconds\n",s,ns);
+    // if(ns < 0L){
+    //     s--;
+    //     ns += 1000000000L;
+    // }
+    ll *out1 = fix(s,ns);
+    printf("Child process 1 (SCHED_OTHER): %lli.%lli seconds\n",*out1, *(out1+1));
 
     s = f2.tv_sec - s2.tv_sec;
     ns = f2.tv_nsec - s2.tv_nsec;
-    if(ns < 0L){
-        s--;
-        ns += 1000000000L;
-    }
-    printf("Child process 2 (SCHED_FIFO): %lli.%lli seconds\n",s,ns);
+    // if(ns < 0L){
+    //     s--;
+    //     ns += 1000000000L;
+    // }
+    ll *out2 = fix(s,ns);
+    printf("Child process 2 (SCHED_FIFO): %lli.%lli seconds\n",*out2, *(out2+1));
 
     s = f3.tv_sec - s3.tv_sec;
     ns = f3.tv_nsec - s3.tv_nsec;
-    if(ns < 0L){
-        s--;
-        ns += 1000000000L;
-    }
-    printf("Child process 3 (SCHED_RR): %lli.%lli seconds\n",s,ns);
+    // if(ns < 0L){
+    //     s--;
+    //     ns += 1000000000L;
+    // }
+    ll *out3 = fix(s,ns);
+    printf("Child process 3 (SCHED_RR): %lli.%lli seconds\n",*out3, *(out3+1));
     
 }
